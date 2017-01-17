b495f308ace63624c7dc5d11b02ca6e04d4d3f82
Steve Mostovoy <stevemostovoysm@gmail.com>
2017-01-16 22:46:04 -0800

    Add connect-local.

--- polly.sh
+++ polly.sh
@@ -7,20 +7,20 @@ po() {
 _po_help() {
     echo "Usage: po <command>"
     echo "Commands:"
-    echo "  help          Print this help dialog."
-    echo "  reload        Reload po."
-    echo "  env           Print docker env (used by 'po connect')."
-    echo "  connect       Point docker to remote stevemostovoy.me host."
-    echo "  disconnect    Point docker to local host."
-    echo "  ssh           SSH into remote stevemostovoy.me host."
-    echo "  shell         Open a shell into the running container."
-    echo "  explore       Run a new container, skipping the entrypoint."
-    echo "  build         Build the docker container."
-    echo "  push          Push the docker image to the docker registry as latest."
-    echo "  pull          Pull the latest images from the docker registry with docker-compose."
-    echo "  attach        Launch the containers with docker-compose and attach to the output."
-    echo "  all           Build and launch with attachment in one command."
-    echo "  up            Launch the containers with docker-compose."
+    echo "  help              Print this help dialog."
+    echo "  reload            Reload po."
+    echo "  env               Print docker env (used by 'po connect')."
+    echo "  connect-local     Point docker to local host."
+    echo "  connect-remote    Point docker to remote stevemostovoy.me host."
+    echo "  ssh               SSH into remote stevemostovoy.me host."
+    echo "  shell             Open a shell into the running container."
+    echo "  explore           Run a new container, skipping the entrypoint."
+    echo "  build             Build the docker container."
+    echo "  push              Push the docker image to the docker registry as latest."
+    echo "  pull              Pull the latest images from the docker registry with docker-compose."
+    echo "  background        Launch the containers with docker-compose in the background."
+    echo "  all               Build and launch with attachment in one command."
+    echo "  up                Launch the containers with docker-compose and attach to the output."
 }
 
 _po_reload() {
@@ -31,11 +31,7 @@ _po_env() {
     docker-machine env stevemostovoy.me
 }
 
-_po_connect() {
-    eval $(docker-machine env stevemostovoy.me)
-}
-
-_po_disconnect() {
+_po_connect-local() {
     unset DOCKER_TLS_VERIFY
     unset DOCKER_HOST
     unset DOCKER_CERT_PATH
@@ -43,6 +39,12 @@ _po_disconnect() {
     unset DOCKER_API_VERSION
     
     export DOCKER_HOST="tcp://localhost:2375"
+    export DIR_PREFIX="c:"
+}
+
+_po_connect-remote() {
+    eval $(docker-machine env stevemostovoy.me)
+    unset DIR_PREFIX
 }
 
 _po_ssh() {
@@ -69,8 +71,8 @@ _po_pull() {
     docker-compose pull
 }
 
-_po_attach() {
-    docker-compose up
+_po_background() {
+    docker-compose up -d
 }
 
 _po_all() {
@@ -79,23 +81,23 @@ _po_all() {
 }
 
 _po_up() {
-    docker-compose up -d
+    docker-compose up
 }
 
 export -f po
 export -f _po_help
 export -f _po_reload
 export -f _po_env
-export -f _po_connect
-export -f _po_disconnect
+export -f _po_connect-local
+export -f _po_connect-remote
 export -f _po_ssh
 export -f _po_shell
 export -f _po_explore
 export -f _po_build
 export -f _po_push
 export -f _po_pull
-export -f _po_attach
-export -f _po_up
+export -f _po_background
 export -f _po_all
+export -f _po_up
 
 echo "polly cli loaded. Use 'po help' for info."
